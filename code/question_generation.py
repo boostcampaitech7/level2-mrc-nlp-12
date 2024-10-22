092ff874effab9a3749318209c6f5f20677ace1b
KKhajiit <junesup0402@gmail.com>
2024-10-22 08:35:00 +0000

    Chore: change model
    - KETI-AIR/ke-t5 models did not work at all (small, base, large)
    
    - changed model to lcw99/t5-base-korean-paraphrase, which made paraphrased questions

--- code/question_generation.py
+++ code/question_generation.py
@@ -1,38 +1,32 @@
-from transformers import T5ForConditionalGeneration, T5Tokenizer
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
 from datasets import Dataset
 import torch
 
-#lcw99/t5-base-korean-paraphrase
-model_name = 'KETI-AIR/ke-t5-large'
+#KETI-AIR/ke-t5-large
+model_name = 'lcw99/t5-base-korean-paraphrase'
 
 # Set device to GPU if available
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-model = T5ForConditionalGeneration.from_pretrained(model_name).to(device) # Move model to GPU
-tokenizer = T5Tokenizer.from_pretrained(model_name)
+model = AutoModelForSeq2SeqLM.from_pretrained(model_name).to(device) # Move model to GPU
+tokenizer = AutoTokenizer.from_pretrained(model_name)
 
 def generate_questions(batch):
     # Prepare inputs for the entire batch
     input_texts = [f"paraphrase: {question}" for question in batch['question']]
-
-    # for context, answer_dict in zip(batch['context'], batch['answers']):
-    #     answer = answer_dict['text']
-    #     input_texts.append(f"generate question: context: {context} answer: {answer}")
-
-    
+  
     # Tokenize the inputs
-    inputs = tokenizer(input_texts, return_tensors='pt', padding=True, truncation=True, max_length=512)
+    inputs = tokenizer(input_texts, return_tensors='pt', padding=True, truncation=True).to(device)
     
-    # Move inputs to the device
-    inputs = {key: val.to(device) for key, val in inputs.items()}
+    # Generate paraphrased questions
+    with torch.no_grad():  # Disable gradient calculations for efficiency
+        outputs = model.generate(**inputs, max_length=64, num_beams=4, early_stopping=True)
 
-    # Generate questions for the entire batch
-    outputs = model.generate(inputs['input_ids'], max_length=64, num_beams=4, early_stopping=True)
-
-    # Decode the generated questions
-    questions = [tokenizer.decode(output, skip_special_tokens=True) for output in outputs]
+    # Decode the generated paraphrases
+    paraphrased_questions = tokenizer.batch_decode(outputs, skip_special_tokens=True)
     
-    return {'question': questions}
+    return {'question': paraphrased_questions}  # Return the new questions
+
 
 
 train_data = Dataset.load_from_disk('../data/train_dataset/train')
@@ -43,4 +37,4 @@ train_data = train_data.map(
     batch_size=8
 )
 
-train_data.save_to_disk('../data/paraphrase_t5_train_dataset')
+train_data.save_to_disk('../data/paraphrase_lcw99t5_train_dataset')
